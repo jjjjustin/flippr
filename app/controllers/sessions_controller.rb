de60da751035690c852690942bf1c24274506901
jjjjustin <justinsitarz@gmail.com>
2015-02-08 19:25:23 -0600

    login and logout stuff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(user_name: params[:session][:user_name].downcase)
+    if user.user_name
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid username'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   resources :users
-
-  get 'users/new'
-  root             'static#home'
-  get 'help'    => 'static#help'
-  get 'about'   => 'static#about'
-  get 'contact' => 'static#contact'
-  get 'signup'  => 'users#new'
+  root   'static#home'
+  get    'sessions/new'
+  get    'users/new'
+  get    'help'    => 'static#help'
+  get    'about'   => 'static#about'
+  get    'contact' => 'static#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
